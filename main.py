2ac7e028d431a9f25c0c56af8eaf34f76b0613c4
Amxzxn-Bxnny <martinezs1376@student.judsonisd.org>
2025-03-21 16:12:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import random
 
+lower_bound = 1
+upper_bound = 100
+number_of_guesses = 0
+
+random_number = random.randint (lower_bound, upper_bound)
+
+guess = int(input("Guess the number: "))
+
+while guess != random_number:
+    if guess == random_number:
+        print("Congradulations! You guess the number correctly.")
+    else:
+        if guess < random_number:
+            print("The number is higher!")
+        else:
+            print("The number is lower!")
+   
+    number_of_guesses += 1
+    guess = int(input("Guess the number: "))
+
+print("Congratulations! You guessed the number correctly in" , number_of_guesses, "attempts.")
